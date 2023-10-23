1ecf9fd5caa2433747bd3e3eab103443e2fe162d
Temurbek_T <102275892+Temur1918@users.noreply.github.com>
2023-10-24 02:51:44 +0500

    Update settings.py

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -142,11 +142,11 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = 'media-files/'
 MEDIA_URL = '/media/'
